ad95ed70a1c78d746606874c810080d943999c83
Matthew Fisher <matthew.fisher@takealot.com>
2019-06-18 12:40:59 +0200

    updated CLI calendar tool

--- cal.py
+++ cal.py
@@ -3,13 +3,15 @@ from datetime import datetime
 import click
 
 @click.command()
-@click.option('--params', '-pr', 'params', is_flag=True)
+@click.option('--year', '-y', 'year', default=datetime.today().year)
+@click.option('--month', '-m', 'month', is_flag=True)
+@click.option('--monthNum', '-mm', 'monthNum', default=datetime.today().month)
 
-def cal(y=datetime.today().year, m=False):
-	if m:
-		print(calendar.month(y,m))
+def cal(year, month, monthNum):
+	if month:
+		print(calendar.month(year,monthNum))
 	else:
-		print(calendar.calendar(y))
+		print(calendar.calendar(year))
 
 if __name__ == '__main__':
 	cal()
