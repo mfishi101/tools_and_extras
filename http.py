5b2ed3e279e5c075476b7ad64c33c121ab50a3c0
Matt Fisher <mfishi101@gmail.com>
2019-07-24 20:11:22 +0200

    http and html examples

---
+++ http.py
@@ -0,0 +1,12 @@
+#launch this in a directory with index.html file and use python produce html website
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
