03358aeb41c3a89928a1370af17e14b30c73df4c
sebwenger <52880079+sebwenger@users.noreply.github.com>
2019-08-08 10:53:44 +0200

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,10 @@
 # Installed Libraries
 from apscheduler.schedulers.background import BackgroundScheduler
 
+# For Windows Stand-Alone Applications (Typically compile with Pyinstaller), environment may not recognise 'cron' ~Seb
+from apscheduler.triggers.cron import CronTrigger
+#---------------------------------------------------------------------------------------------------------------------
+
 def someRandomFunction():
     """some random code"""
 
@@ -11,6 +15,11 @@ if __name__ == '__main__':
     scheduler = BackgroundScheduler()
     scheduler.add_job(someRandomFunction, trigger='cron', day_of_week='mon', hour=2, minute=0, second=0)
 
+    #For Stand alone app --------------------------------------------------------------------------------------------
+    args = ['Arg1', 2 , [1.2,3.45,67.89]]   # example, if function takes in argument varibles, lists, objects etc.
+    trigger = CronTrigger(day_of_week='mon', hour=2, minute=0, second=0)
+    sched.add_job(someRandomFunction, trigger, args=(args[0],args[2],args[2])   #can leave out args overload if there are no arguments in function
+    #----------------------------------------------------------------------------------------------------------------
     scheduler.start()
 
     try:
