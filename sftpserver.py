6683603baf7a8a20fe67b3b03442714669dd7e4d
Matt Fisher <matthew.fisher@takealot.com>
2019-10-13 17:55:30 +0200

    add ftp port 21 option

--- sftpserver.py
+++ sftpserver.py
@@ -17,3 +17,22 @@ class Sftpserve:
 				sftp.chmod(self.outputfile, self.mode)
 				# Closes the connection
 		sftp.close()
+
+# option for ftp (port 21) as well
+# can just wrap this in a function like above and make dynamic for calling in multiple areas
+
+from ftplib import FTP 
+import os
+
+ftp = FTP()
+ftp.set_debuglevel(2)
+ftp.connect('ftphostname', 21) 
+ftp.login('username','password')
+
+localfile = 'path_of_file_to_upload'
+
+fp = open(localfile, 'rb')
+
+ftp.storbinary('STOR %s' % os.path.basename(localfile), fp, 1024)
+fp.close()
+ftp.quit()
